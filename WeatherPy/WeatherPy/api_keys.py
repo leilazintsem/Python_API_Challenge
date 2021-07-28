8b3c0815016a1df2af0c176f20d99cf50f71359d
Leila zintsem a Moute <77307283+leilazintsem@users.noreply.github.com>
2021-07-28 16:21:31 -0500

    Update api_keys.py

--- WeatherPy/WeatherPy/api_keys.py
+++ WeatherPy/WeatherPy/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "b6ab7dc73585b48469f752defd783185"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyA8UkyvwY7gc3kTdG9W2fbtZTlhbH0730k"
+g_key = ""
+Enter your api key here
